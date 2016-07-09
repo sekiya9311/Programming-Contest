742c4b5bcd862c7c22d6db1de45f708f1dc3acae
Sekiya <sekiya9311@gmail.com>
2016-07-09 23:17:26 +0900

    ARC057

---
+++ Atcoder/ARC057/A.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+#define REP(i,n) for(long (i)=0;(i)<(n);(i)++)
+#define FOR(i,a,b) for(long (i)=(a);(i)<(b);(i)++)
+#define RREP(i,a) for(long (i)=(a)-1;(i)>=0;(i)--)
+#define FORR(i,a,b) for(long (i)=(a)-1;(i)>=(b);(i)--)
+#define PI acos(-1.0)
+#define DEBUG(C) cout<<C<<endl;
+#define VI vector<int>
+#define VL vector<long>
+#define VD vector<double>
+#define PII pair<int,int>
+#define PDD pair<double,double>
+#define PLL pair<long,long>
+#define ALL(a) (a).begin(),(a).end()
+#define SORT(a) sort(ALL(a))
+#define REVERSE(a) reverse(ALL(a))
+#define MP make_pair
+#define FORE(a,b) for(auto &&a:b)
+
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+const int INF=1e9;
+const int MOD=INF+7;
+
+const LL N=2*(1e12);
+
+int main(void){
+    long A,K; cin>>A>>K;
+    long cnt=0;
+    if(K==0){
+        cout<<N-A<<endl;
+        return 0;
+    }
+    while(A<N){
+        A+=1+A*K;
+        cnt++;
+    } cout<<cnt<<endl;
+    return 0;
+}

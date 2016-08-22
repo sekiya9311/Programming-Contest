3ed43e27f1294e04d8a77c2761557907b2a86ed4
Sekiya <sekiya9311@gmail.com>
2016-08-22 09:31:45 +0100

    AGC003

---
+++ Atcoder/AGC003/B.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+#define REP(i,n) for(int (i)=0;(i)<(n);(i)++)
+#define FOR(i,a,b) for(int (i)=(a);(i)<(b);(i)++)
+#define RREP(i,a) for(int (i)=(a)-1;(i)>=0;(i)--)
+#define FORR(i,a,b) for(int (i)=(a)-1;(i)>=(b);(i)--)
+#define PI acos(-1.0)
+#define DEBUG(C) cout<<C<<endl;
+#define VI vector<int>
+#define VII vector<VI>
+#define VL vector<LL>
+#define VLL vector<VL>
+#define VD vector<double>
+#define VDD vector<VD>
+#define PII pair<int,int>
+#define PDD pair<double,double>
+#define PLL pair<LL,LL>
+#define VPII vector<PII>
+#define ALL(a) (a).begin(),(a).end()
+#define SORT(a) sort(ALL(a))
+#define REVERSE(a) reverse(ALL(a))
+#define MP make_pair
+#define EP emplace_back
+#define FORE(a,b) for(auto &&a:b)
+
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+const int INF=1e9;
+const int MOD=INF+7;
+#define int LL
+
+signed main(){
+    int N; cin >> N;
+    VL A(N);
+    REP(i,N){
+        scanf("%lld",&A[i]);
+    }
+    VL dp(N,0);
+    dp[0]=A[0]/2;
+    A[0]-=(dp[0]*2);
+    FOR(i,1,N){
+        if(A[i]&&A[i-1]){
+            A[i]--;A[i-1]--;
+            dp[i]++;
+        }
+        int buf=A[i]/2;
+        A[i]-=(buf*2);
+        dp[i]+=dp[i-1]+buf;
+    }
+    cout<<dp.back()<<endl;
+}

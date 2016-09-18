e0990936bc7c4cca8226f44d9aee451818637ecd
sekiya9311 <sekiya9311@gmail.com>
2016-09-18 19:26:00 +0900

    2016/09/18

--- codeforces/CFR#372/D.cpp
+++ codeforces/CFR#372/D.cpp
@@ -48,29 +48,31 @@
 #define MP make_pair
 #define FORE(a, b) for (auto &&a:b)
 #define FIND(s, e) (s.find(e) != s.end())
+#define EB emplace_back
 
 using namespace std;
 typedef long long LL;
 typedef unsigned long long ULL;
 const int INF = 1e9;
 const int MOD = INF+7;
-const LL LLINF = 1e18;
+const LL LLINF = 1e14;
 #define int LL
 
 /** NOT AC **/
 
 LL n, m, L, s, t;
-vector<VPII> graph;
-LL dijkstra() {
+vector<VPII> graph_min, graph_max;
+
+LL dijkstra(const vector<VPII> &graph) {
     LL res = LLINF;
     vector <LL> minCost(n, LLINF);
-    priority_queue<PLL,vector<PLL>,greater<PLL> > q;
+    priority_queue<PLL, vector<PLL>, greater<PLL>> q;
     q.push(MP(0, s));
     while(!q.empty()) {
         auto now = q.top(); q.pop();
         LL pos = now.second, cost = now.first;
         if (pos == t) {
-            res = min(res, cost);
+            return cost;
         }
         if (minCost[pos] == LLINF) minCost[pos] = cost;
         else continue;
@@ -83,39 +85,57 @@ LL dijkstra() {
 
 signed main(void) {
     cin >> n >> m >> L >> s >>t;
-    graph = vector<VPII>(n);
-    vector <tuple<int, int, int>> ans(m);
+    graph_min = vector<VPII>(n);
+    graph_max = vector<VPII>(n);
+    vector <tuple <int, int, int>> ans(m);
+    VPII zero;  //zero_edge
     REP(i, m) {
         int a, b, cost;
         scanf("%lld%lld%lld", &a, &b, &cost);
-        //cout << a << " " << b << " " << cost << endl;
         ans[i] = make_tuple(a, b, cost);
-        graph[a].emplace_back(b, cost);
-        graph[b].emplace_back(a, cost);
-        //cout << get<0>(ans[i]) << " " << get<1>(ans[i]) << " " << get<2>(ans[i]) << endl;
+        graph_min[a].EB(MP(b, ((cost == 0)?1:cost)));
+        graph_min[b].EB(MP(a, ((cost == 0)?1:cost)));
+        graph_max[a].EB(MP(b, ((cost == 0)?LLINF:cost)));
+        graph_max[b].EB(MP(a, ((cost == 0)?LLINF:cost)));
+        if (cost == 0) zero.EB(a, b);
     }
-    LL ret = dijkstra();
-    if (ret > L) {
+    LL ret_min = dijkstra(graph_min);
+    LL ret_max = dijkstra(graph_max);
+    if (L < ret_min || ret_max < L) {
         cout << "NO" << endl;
         return 0;
     }
-    cout << "YES" << endl;
-    LL lc = L - ret;
-    LL cnt = 0;
-    REP(i, m) {
-        if (get<2>(ans[i]) == 0) {
-            cnt++;
-        }
-    }
-    REP(i, m) {
-        if (get<2>(ans[i]) == 0) {
-            if (--cnt) {
-                ans[i] = make_tuple(get<0>(ans[i]), get<1>(ans[i]), 1);
-                lc--;
-            } else {
-                ans[i] = make_tuple(get<0>(ans[i]), get<1>(ans[i]), lc);
+    PII ansp = MP(0, 0);
+    LL ansc = 0;
+    set<PII> se;
+    if (L < ret_max) FORE(p, zero) {
+        FORE(el, graph_max[p.first]) {
+            if (el.first == p.second) {
+                el.second = 1;
+                break;
             }
         }
-        printf("%lld %lld %lld\n", get<0>(ans[i]), get<1>(ans[i]), get<2>(ans[i]));
+        FORE(el, graph_max[p.second]) {
+            if (el.first == p.first) {
+                el.second = 1;
+                break;
+            }
+        }
+        LL ret = dijkstra(graph_max);
+        if (ret <= L) {
+            ansp = p;
+            ansc = 1 + L - ret;
+            break;
+        }
+        se.insert(p);
+    }
+    cout << "YES" << endl;
+    FORE(el, ans) {
+        if (get<2>(el) == 0) {
+            if (ansp == MP(get<0>(el), get<1>(el))) el = make_tuple(get<0>(el), get<1>(el), ansc);
+            else if (FIND(se, MP(get<0>(el), get<1>(el)))) el = make_tuple(get<0>(el), get<1>(el), 1);
+            else el = make_tuple(get<0>(el), get<1>(el), LLINF);
+        }
+        printf("%lld %lld %lld\n", get<0>(el), get<1>(el), get<2>(el));
     }
 }

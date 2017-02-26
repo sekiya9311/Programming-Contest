1ec53f080a796e24ac68f2a88b3c4164ad7cd0df
sekiya9311 <sekiya9311@gmail.com>
2017-02-26 23:37:10 +0900

    CFR#402

---
+++ codeforces/CFR#402/D.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 200010;
+char strbuf[MAX];
+string T, P;
+int tlen;
+int a[MAX];
+unordered_set<int> se;
+
+int main(void) {
+    scanf("%s", strbuf);
+    T = strbuf;
+    scanf("%s", strbuf);
+    P = strbuf;
+    tlen = T.length();
+    REP(i, tlen) {
+        scanf("%d", a + i);
+        a[i]--;
+    }
+    int high = tlen, low = 0;
+    while (high - low > 1) {
+        const int mid = (high + low) / 2;
+        REP(i, mid) {
+            se.insert(a[i]);
+        }
+        int now = 0;
+        REP(i, tlen) {
+            if (T[i] == P[now] && !FIND(se, i)) now++;
+            if (now == (int)P.length()) break;
+        }
+        if (now == (int)P.length()) low = mid;
+        else high = mid;
+        se.clear();
+    }
+    cout << low << endl;
+}

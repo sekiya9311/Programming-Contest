9c4b00dde396da1b16fdeb66d1bbcac9dfda82e8
sekiya9311 <sekiya9311@gmail.com>
2017-04-23 00:10:58 +0900

    ARC072

---
+++ Atcoder/ARC072/C.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 1e5 + 10;
+int n;
+long long a[MAX];
+pair<LL, LL> dp[MAX][2];
+// (j == 0) +, (j == 1) -
+
+int main(void) {
+    scanf("%d", &n);
+    REP(i, n) {
+        scanf("%lld", a + i);
+    }
+    //REP(i, MAX)REP(j, 2) dp[i][j] = MP(LLINF, LLINF);
+    if (a[0] == 0) {
+        dp[0][0] = MP(1, 1);
+        dp[0][1] = MP(-1, 1);
+    } else if (a[0] > 0) {
+        dp[0][0] = MP(a[0], 0);
+        dp[0][1] = MP(-1, a[0] + 1);
+    } else {
+        dp[0][1] = MP(a[0], 0);
+        dp[0][0] = MP(1, -a[0] + 1);
+    }
+    FOR(i, 1, n) {
+        REP(j, 2) {
+            if (a[i] + dp[i - 1][j].first == 0) {
+                if (j == 0) {
+                    dp[i][1] = MP(-1, dp[i - 1][j].second + 1);
+                } else {
+                    dp[i][0] = MP(1, dp[i - 1][j].second + 1);
+                }
+            } else if (a[i] + dp[i - 1][j].first > 0) {
+                if (j == 0) {
+                    dp[i][1] = MP(-1, dp[i - 1][j].second + a[i] + dp[i - 1][j].first + 1);
+                } else {
+                    dp[i][0] = MP(a[i] + dp[i - 1][j].first, dp[i - 1][j].second);
+                }
+            } else {
+                if (j == 0) {
+                    dp[i][1] = MP(a[i] + dp[i - 1][j].first, dp[i - 1][j].second);
+                } else {
+                    dp[i][0] = MP(1, dp[i - 1][j].second - (a[i] + dp[i - 1][j].first) + 1);
+                }
+            }
+        }
+    }
+    cout << min(dp[n - 1][0].second, dp[n - 1][1].second) << endl;
+}

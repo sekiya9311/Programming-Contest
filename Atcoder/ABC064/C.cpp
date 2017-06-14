be3a2d673e7424ac7d263279b4f7f266059511e1
Sekiya <sekiya9311@gmail.com>
2017-06-14 22:19:35 +0900

    ABC064

---
+++ Atcoder/ABC064/C.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 111;
+int N, a[MAX];
+
+int ma() {
+    bool cnt[8];
+    REP(i, 8) cnt[i] = false;
+    int free = 0;
+    REP(i, N) {
+        if (a[i] <= 399) cnt[0] = true;
+        else if (a[i] <= 799) cnt[1] = true;
+        else if (a[i] <= 1199) cnt[2] = true;
+        else if (a[i] <= 1599) cnt[3] = true;
+        else if (a[i] <= 1999) cnt[4] = true;
+        else if (a[i] <= 2399) cnt[5] = true;
+        else if (a[i] <= 2799) cnt[6] = true;
+        else if (a[i] <= 3199) cnt[7] = true;
+        else free++;
+    }
+    int res = 0;
+    REP(i, 8) res += cnt[i];
+    return res + free;
+}
+int mi() {
+    bool cnt[8];
+    REP(i, 8) cnt[i] = false;
+    bool free = false;
+    REP(i, N) {
+        if (a[i] <= 399) cnt[0] = true;
+        else if (a[i] <= 799) cnt[1] = true;
+        else if (a[i] <= 1199) cnt[2] = true;
+        else if (a[i] <= 1599) cnt[3] = true;
+        else if (a[i] <= 1999) cnt[4] = true;
+        else if (a[i] <= 2399) cnt[5] = true;
+        else if (a[i] <= 2799) cnt[6] = true;
+        else if (a[i] <= 3199) cnt[7] = true;
+        else free = true;
+    }
+    int res = 0;
+    REP(i, 8) res += cnt[i];
+    if (res == 0 && free) ++res;
+    return res;
+}
+
+int main(void) {
+    cin >> N;
+    REP(i, N) {
+        cin >> a[i];
+    }
+    cout << mi() << " " << ma() << endl;
+}

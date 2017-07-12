f962fe62655723c21870b53072cdc2d84738921b
sekiya9311 <sekiya9311@gmail.com>
2017-07-12 21:26:48 +0900

    AGC017

---
+++ Atcoder/AGC017/D.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 100010;
+int N;
+int x[MAX], y[MAX];
+VI graph[MAX];
+
+int dfs(int now, int prev) {
+    int res = 0;
+    FORE(e, graph[now]) {
+        if (e != prev) {
+            res ^= dfs(e, now) + 1;
+        }
+    }
+    return res;
+}
+
+int main(void) {
+    scanf("%d", &N);
+    int oneCnt = 0;
+    REP(i, N - 1) {
+        scanf("%d%d", x + i, y + i);
+        oneCnt += ((x[i] == 1) || (y[i] == 1));
+        x[i]--;
+        y[i]--;
+        graph[x[i]].EB(y[i]);
+        graph[y[i]].EB(x[i]);
+    }
+    if (oneCnt == 1) {
+        cout << "Alice" << endl;
+        return 0;
+    }
+    else {
+        cout << (dfs(0, -1) ? "Alice" : "Bob") << endl;
+    }
+}

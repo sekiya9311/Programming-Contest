91f7639b26608cd053422ba79055199dd2316f19
sekiya9311 <sekiya9311@gmail.com>
2017-08-19 15:22:14 +0900

    CFR#429

---
+++ codeforces/CFR#429/C.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 2e5 + 10;
+int m;
+LL a[MAX], b[MAX];
+LL ans[MAX];
+
+int main(void) {
+    scanf("%d", &m);
+    REP(i, m) {
+        scanf("%lld", a + i);
+    }
+    sort(a, a + m);
+    priority_queue<pair<LL, int>, VT<pair<LL, int>>, greater<pair<LL, int>>> pq;
+    REP(i, m) {
+        scanf("%lld", b + i);
+        pq.push(MP(b[i], i));
+    }
+    RREP(i, m) {
+        ans[pq.top().second] = a[i];
+        pq.pop();
+    }
+
+    REP(i, m) {
+        printf("%lld%c", ans[i], (i + 1 == m ? '\n' : ' '));
+    }
+}

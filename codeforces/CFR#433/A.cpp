95abe26caca2a44326ed1e4f5f87df141e928c3c
sekiya9311 <sekiya9311@gmail.com>
2017-09-07 00:50:50 +0900

    CFR#433

---
+++ codeforces/CFR#433/A.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+long long gcd(long long a, long long b) {
+    if(a < b) swap(a, b);
+    long long ret = 1;
+    while(b) {ret = b; b = a % b; a = ret;}
+    return ret;
+}
+
+int main(void) {
+    int n;
+    cin >> n;
+    VPII v;
+    RREP(i, n) {
+        int a = i;
+        int b = n - a;
+        if (a != 0 && b != 0 && a < b && gcd(a, b) == 1) {
+            v.EB(MP(a, b));
+        }
+    }
+    long double ma = 0;
+    int a = -1, b = -1;
+    FORE(e, v) {
+        if (chmax(ma, (long double) e.first / e.second)) {
+            a = e.first;
+            b = e.second;
+        }
+    }
+    cout << a << " " << b << endl;
+}

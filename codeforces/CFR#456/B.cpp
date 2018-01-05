55ae2e94269c762a0c3b5e6090abf77bb75d6ecb
sekiya9311 <sekiya9311@gmail.com>
2018-01-06 02:38:16 +0900

    CFR#456

---
+++ codeforces/CFR#456/B.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+LL K, N;
+
+int main(void) {
+    cin >> N >> K;
+    if (K == 1) {
+        cout << N << endl;
+        return 0;
+    }
+    int msb = 0;
+    {
+        REP(i, 60) {
+            if (N & (1ll << i)) {
+                msb = i;
+            }
+        }
+    }
+    LL ma = (1ll << (msb + 1)) - 1;
+    cout << ma << endl;
+    return 0;
+    int secMsb = 0;
+    REP(i, msb) {
+        if (1 & (1ll << i)) {
+            secMsb = i;
+        }
+    }
+    LL ans = (1LL << msb) | ((1LL << secMsb) - 1);
+    RREP(i, msb) {
+        if ((ans & (1ll << i)) == 0 && K) {
+            K--;
+            ans |= (1ll << i);
+        }
+    }
+    cout << max(ans, N) << endl;
+}

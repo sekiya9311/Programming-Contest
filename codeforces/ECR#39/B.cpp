46688c9332423155e88af6e17bb699c5f368f6e5
sekiya9311 <sekiya9311@gmail.com>
2018-03-08 01:20:06 +0900

    ECR#39

---
+++ codeforces/ECR#39/B.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+LL A, B;
+
+LL calc(LL N, LL M) {
+    LL high = 1e18 / (2 * M) + 1, low = 0;
+    while (high - low > 1) {
+        LL mid = low + (high - low) / 2;
+        if (N - mid * M * 2 >= M * 2) low = mid;
+        else high = mid;
+    }
+    return high * (M * 2);
+}
+
+int main(void) {
+    cin >> A >> B;
+    while (A > 0 && B > 0) {
+        if (A >= 2 * B) {
+            A -= calc(A, B);
+        } else if (B >= 2 * A) {
+            B -= calc(B, A);
+        } else {
+            break;
+        }
+    }
+    cout << A << " " << B << endl;
+}
---
+++ codeforces/ECR#39/D.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 555;
+int N, M, K;
+vector<int> memo[MAX];
+int dp[MAX][MAX];
+
+int main(void) {
+    cin >> N >> M >> K;
+    REP(i, N) {
+        string s;
+        cin >> s;
+        vector<int> st;
+        REP(j, (int)s.size()) {
+            if (s[j] == '1') {
+                st.EB(j);
+            }
+        }
+        memo[i].resize(st.size() + 1, INF);
+        memo[i][st.size()] = 0;
+        REP(j, (int)st.size()) {
+            FOR(k, j + 1, (int)st.size() + 1) {
+                chmin(memo[i][st.size() - (k - j)], st[k - 1] - st[j] + 1);
+            }
+        }
+    }
+    REP(i, MAX) REP(j, MAX) dp[i][j] = INF;
+    dp[0][0] = 0;
+    REP(i, N) {
+        REP(j, K + 1) {
+            REP(k, (int)memo[i].size()) {
+                if (j + k <= K) {
+                    chmin(dp[i + 1][j + k], dp[i][j] + memo[i][k]);
+                }
+            }
+        }
+    }
+    int ans = INF;
+    REP(i, MAX) chmin(ans, dp[N][i]);
+    cout << ans << endl;
+}

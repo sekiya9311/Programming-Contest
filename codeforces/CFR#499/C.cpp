2cc5d22df1b8145fe7b246a33d1a609fcf2fe103
sekiya9311 <sekiya9311@gmail.com>
2018-07-27 02:12:52 +0900

    CFR#499

---
+++ codeforces/CFR#499/C.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+int N, M;
+int a[1111], b[1111];
+
+long double calc(long double fuel) {
+    REP(i, N) {
+        fuel -= (M + fuel) / a[i];
+        if (fuel < 0) return -1;
+        fuel -= (M + fuel) / b[i];
+        if (fuel < 0) return -1;
+    }
+    return fuel;
+}
+
+int main(void) {
+    cin >> N >> M;
+    REP(i, N) cin >> a[i];
+    REP(i, N) cin >> b[i];
+    long double high = INF, low = 0;
+    REP(i, 111) {
+        const double mid = (high + low) / 2;
+        if (calc(mid) == -1) {
+            low = mid;
+        } else {
+            high = mid;
+        }
+    }
+    if (calc(high) == -1)
+        cout << -1 << endl;
+    else
+        cout << fixed << setprecision(10) <<  high << endl;
+}

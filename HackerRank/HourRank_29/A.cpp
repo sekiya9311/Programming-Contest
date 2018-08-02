0494a91e9372ef9d2519c7f0ada2130e1de14560
sekiya9311 <sekiya9311@gmail.com>
2018-08-03 01:08:21 +0900

    HourRank_29

---
+++ HackerRank/HourRank_29/A.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int dx[] = {0, -1, 0, 1};
+const int dy[] = {-1, 0, 1, 0};
+
+int main(void) {
+    int t;
+    cin >> t;
+    REP(_, t) {
+        int n;
+        cin >> n;
+        VVI a(n, VI(n));
+        REP(i, n) REP(j, n) cin >> a[i][j];
+        bool ok = true;
+        REP(i, n) {
+            REP(j, n) {
+                REP(k, 4) {
+                    int ni = i + dx[k];
+                    int nj = j + dy[k];
+                    if (0 <= ni && ni < n && 0 <= nj && nj < n) {
+                        ok &= a[i][j] != a[ni][nj];
+                    }
+                }
+            }
+        }
+        cout << (ok ? "Yes" : "No") << endl;
+    }
+}

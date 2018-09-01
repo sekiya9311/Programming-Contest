b79751cf5b3a6421d57588a3de1090df89b8e34a
sekiya9311 <sekiya9311@gmail.com>
2018-09-01 23:59:39 +0900

    ARC102

---
+++ Atcoder/ARC102/C.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+int N, K;
+
+void naive() {
+    LL ans = 0;
+    FOR(i, 1,  N + 1) {
+        FOR(j, 1, N + 1) {
+            FOR(k, 1, N + 1) {
+                if ((i + j) % K == 0 &&
+                    (j + k) % K == 0 &&
+                    (k + i) % K == 0) {
+                    ans++;
+                }
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+int main(void) {
+    cin >> N >> K;
+    LL ans = 0;
+    const auto foo = N / K;
+    if (K % 2 == 0) {
+        // all (mod K == k/2) count
+        if (N % K >= K / 2) {
+            ans += powl(foo + 1, 3);
+        } else {
+            ans += powl(foo, 3);
+        }
+    }
+    // all (mod K == 0) count
+    ans += powl(foo, 3);
+    cout << ans << endl;
+}

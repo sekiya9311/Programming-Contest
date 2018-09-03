5973537fa1b2b76fda3d959f14129345ef29b7ea
sekiya9311 <sekiya9311@gmail.com>
2018-09-03 21:17:31 +0900

    Manthan_Codefest_18

---
+++ codeforces/Manthan_Codefest_18/C.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 1e6 + 10;
+char strbuf[MAX];
+inline string next() {scanf("%s", strbuf);return strbuf;}
+
+int N;
+string A, B;
+
+bool f(int i) {
+    if (A[i] != A[i + 1]) {
+        if (A[i] != B[i] && A[i + 1] != B[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(void) {
+    scanf("%d", &N);
+    A = next();
+    B = next();
+    LL ans = 0;
+    REP(i, N) {
+        if (i + 1 < N && f(i)) {
+            ans++;
+            i++;
+        } else if (A[i] != B[i]) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}

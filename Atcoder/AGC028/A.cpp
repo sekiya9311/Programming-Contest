61acd503a2e41c55bec1547d1e5373b7e7075cf6
sekiya9311 <sekiya9311@gmail.com>
2018-10-15 23:40:03 +0900

    AGC028

---
+++ Atcoder/AGC028/A.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+int main(void) {
+    LL N, M;
+    cin >> N >> M;
+    string S, T;
+    cin >> S >> T;
+    if (S[0] != T[0]) {
+        cout << -1 << endl;
+        return 0;
+    }
+    LL len = N;
+    LL cnt = 1;
+    LL ma = N / __gcd(N, M) * M;
+    while (len <= ma) {
+        if (len % M == 0) {
+            bool ok = true;
+            REP(i, N) {
+                LL s_idx = i * cnt;
+                if (s_idx % (len / M) == 0 && S[i] != T[s_idx / (len / M)]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                cout << len << endl;
+                return 0;
+            }
+        }
+        len += N;
+        cnt++;
+    }
+
+    cout << -1 << endl;
+}

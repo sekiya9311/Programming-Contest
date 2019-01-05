a3a3b9b871ab30a969c09398f4013e77528f9fec
sekiya9311 <sekiya9311@gmail.com>
2019-01-05 14:27:00 +0900

    Hello_2019

---
+++ codeforces/Hello_2019/C.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define FIND(s, e) ((s).find(e) != (s).end())
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX_N = 1e5 + 10;
+const int MAX_L = 5e5 + 10;
+string next() {
+    static char strbuf[MAX_L];
+    scanf("%s", strbuf);
+    return strbuf;
+}
+int N;
+string S[MAX_N];
+unordered_map<int, unordered_set<int>> idxs_l, idxs_r, idxs_all;
+
+int main(void) {
+    scanf("%d", &N);
+    REP(i, N) {
+        S[i] = next();
+    }
+    int ans = 0;
+    REP(i, N) {
+        int cnt = 0;
+        bool l_ok = true, r_ok = true;
+        FORE(e, S[i]) {
+            if (e == '(') {
+                cnt++;
+            } else {
+                cnt--;
+                if (cnt < 0) l_ok = false;
+            }
+        }
+        cnt = 0;
+        RREP(j, S[i].size()) {
+            char c = S[i][j];
+            if (c == '(') {
+                cnt++;
+                if (cnt > 0) r_ok = false;
+            } else {
+                cnt--;
+            }
+        }
+        if (S[i][0] == '(' && l_ok && S[i].back() == ')' && r_ok) {
+            idxs_all[cnt].emplace(i);
+        } else if (S[i][0] == '(' && l_ok) {
+            if (idxs_r[-cnt].size()) {
+                idxs_r[-cnt].erase(idxs_r[-cnt].begin());
+                ans++;
+            } else {
+                idxs_l[cnt].emplace(i);
+            }
+        } else if (S[i].back() == ')' && r_ok) {
+            if (idxs_l[-cnt].size()) {
+                idxs_l[-cnt].erase(idxs_l[-cnt].begin());
+                ans++;
+            } else {
+                idxs_r[cnt].emplace(i);
+            }
+        }
+    }
+
+    FORE(p, idxs_l) {
+        FORE(e, p.second) {
+            if (idxs_all[-p.first].size()) {
+                ans++;
+                idxs_all[-p.first].erase(idxs_all[-p.first].begin());
+            } else {
+                break;
+            }
+        }
+    }
+
+    FORE(p, idxs_r) {
+        FORE(e, p.second) {
+            if (idxs_all[-p.first].size()) {
+                ans++;
+                idxs_all[-p.first].erase(idxs_all[-p.first].begin());
+            } else {
+                break;
+            }
+        }
+    }
+
+    FORE(p, idxs_all) {
+        if (p.first == 0) {
+            ans += p.second.size() / 2;
+        } else {
+            VI v;
+            FORE(e, p.second) {
+                if (idxs_all[-p.first].size()) {
+                    ans++;
+                    idxs_all[-p.first].erase(idxs_all[-p.first].begin());
+                    v.EB(e);
+                } else {
+                    break;
+                }
+            }
+            FORE(e, v) {
+                idxs_all[p.first].erase(e);
+            }
+        }
+    }
+
+    cout << ans << endl;
+}

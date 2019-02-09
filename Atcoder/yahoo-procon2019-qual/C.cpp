e9751a945edd843b7ca2e897308a88b986fb0262
sekiya9311 <sekiya9311@gmail.com>
2019-02-09 23:02:43 +0900

    yahoo-procon2019-qual

---
+++ Atcoder/yahoo-procon2019-qual/C.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+LL K, A, B;
+
+int main(void) {
+    cin >> K >> A >> B;
+    LL ans = K + 1;
+    if (A >= K) {
+        DEBUG("A")
+        cout << K + 1 << endl;
+    } else if (A < B) {
+        LL tmp = K - (A - 1);
+        if (tmp % 2 == 0) {
+            DEBUG("B")
+            cout << max((tmp / 2) * B - (tmp - 1) / 2 * A, ans) << endl;
+        } else {
+            DEBUG("C")
+            cout << max((tmp - 1) / 2 * B - (tmp - 2) / 2 * A + 1, ans) << endl;
+        }
+    } else {
+        DEBUG("D")
+        cout << K + 1 << endl;
+    }
+}

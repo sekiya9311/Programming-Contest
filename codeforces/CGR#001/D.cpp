30f155e5c866f522657dfe2b87d3b01cef23112f
sekiya9311 <sekiya9311@gmail.com>
2019-02-10 23:00:56 +0900

    CGR#001_D

---
+++ codeforces/CGR#001/D.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define EB emplace_back
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 1e6 + 10;
+int N, M;
+int a[MAX];
+int dp[MAX][3][3];
+
+int main(void) {
+    scanf("%d%d", &N, &M);
+    REP(i, N) {
+        int v;
+        scanf("%d", &v);
+        v--;
+        a[v]++;
+    }
+    memset(dp, -1, sizeof(dp));
+    dp[0][0][0] = 0;
+    REP(i, M) {
+        const int cnt = a[i];
+        REP(j, 3) {
+            REP(k, 3) {
+                if (dp[i][j][k] == -1) continue;
+                REP(l, 3) {
+                    const int tmp = cnt - (j + k + l);
+                    if (tmp < 0) continue;
+                    chmax(dp[i + 1][k][l], dp[i][j][k] + l + tmp / 3);
+                }
+            }
+        }
+    }
+    cout << dp[M][0][0] << endl;
+}

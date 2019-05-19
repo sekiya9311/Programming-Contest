f2fd851032dbc8763e788f9e9fb710225a08688f
sekiya9311 <sekiya9311@gmail.com>
2019-05-20 00:03:13 +0900

    ABC126

---
+++ Atcoder/ABC126/C.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define EB emplace_back
+#define GREATER(T) T, VT<T> greater<T>
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+int N, K;
+
+const int MAX = 1e5 + 10;
+long double per_2[MAX];
+
+int main(void) {
+    cin >> N >> K;
+    long double ans = 0;
+
+    per_2[0] = (1.0 / N);
+    per_2[1] = (1.0 / N) * (1.0 / 2);
+    FOR(i, 2, MAX) {
+        per_2[i] = per_2[i - 1] / 2;
+    }
+
+    FOR(i, 1, N + 1) {
+        int cnt = 0;
+        int tmp_i = i;
+        while (tmp_i < K) {
+            tmp_i *= 2;
+            cnt++;
+        }
+        ans += per_2[cnt];
+    }
+
+    cout << fixed << setprecision(20) << ans << endl;
+}

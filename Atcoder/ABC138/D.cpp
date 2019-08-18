58bf518faeab8f439df874fd2ce9acff4c3a2a50
sekiya9311 <sekiya9311@gmail.com>
2019-08-18 22:49:01 +0900

    ABC138

---
+++ Atcoder/ABC138/D.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define EB emplace_back
+#define GREATER(T) T, VT<T> greater<T>
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 2e5 + 10;
+int N, Q;
+int a[MAX], b[MAX];
+int p[MAX], x[MAX];
+
+VI G[MAX];
+LL cnt[MAX];
+LL ans[MAX];
+
+void dfs(int cur, int par) {
+    const LL prev_cnt = (par == -1 ? 0 : ans[par]);
+    const LL cur_cnt = prev_cnt + cnt[cur];
+    ans[cur] = cur_cnt;
+    FORE(e, G[cur]) if (e != par) {
+        dfs(e, cur);
+    }
+}
+
+int main(void) {
+    scanf("%d%d", &N, &Q);
+    REP(i, N - 1) {
+        scanf("%d%d", a + i, b + i);
+        a[i]--; b[i]--;
+        G[a[i]].EB(b[i]);
+        G[b[i]].EB(a[i]);
+    }
+    REP(i, Q) {
+        scanf("%d%d", p + i , x + i);
+        p[i]--;
+        cnt[p[i]] += x[i];
+    }
+
+    dfs(0, -1);
+
+    REP(i, N) {
+        printf("%lld%c", ans[i], (i + 1 == N ? '\n' : ' '));
+    }
+}
---
+++ Atcoder/ABC138/E.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define EB emplace_back
+#define GREATER(T) T, VT<T> greater<T>
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+string S, T;
+
+bool valid() {
+    set<char> se;
+    FORE(e, S) se.emplace(e);
+    FORE(e, T) if (se.count(e) == 0) {
+        return false;
+    }
+    return true;
+}
+
+set<int> init_idx[26];
+int get_nxt(int val, int prev) {
+    auto itr = init_idx[val].upper_bound(prev);
+    if (itr == end(init_idx[val])) {
+        return *begin(init_idx[val]);
+    } else {
+        return *itr;
+    }
+}
+
+int main(void) {
+    cin >> S;
+    cin >> T;
+    if (!valid()) {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    REP(i, (int)S.length()) {
+        init_idx[S[i] - 'a'].emplace(i);
+    }
+
+    LL ans = 0;
+    int prev = -1;
+    FORE(e, T) {
+        const int cur_val = e - 'a';
+
+        const auto nxt = get_nxt(cur_val, prev);
+        assert(S[nxt] == e);
+        if (prev < nxt) {
+            ans += nxt - prev;
+            prev = nxt;
+        } else {
+            ans += (int)S.length() - 1 - prev;
+            ans += nxt + 1;
+            prev = nxt;
+        }
+
+        // DEBUG(ans)
+    }
+
+    cout << ans << endl;
+}

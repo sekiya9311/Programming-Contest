87c472e2eb0c07920a483312f2d2d5c9f1d88c3b
sekiya9311 <sekiya9311@gmail.com>
2019-11-25 00:10:50 +0900

    ABC146

---
+++ Atcoder/ABC146/F.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define EB emplace_back
+#define GREATER(T) T, VT<T> greater<T>
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 1e5 + 10;
+int N, M;
+string S;
+int r[MAX];
+
+int main(void) {
+    cin >> N >> M;
+    cin >> S;
+
+    {
+        int pre = N + 1;
+        RREP(i, N + 1) {
+            if (S[i] == '0') {
+                if (pre - i > M) {
+                    cout << -1 << endl;
+                    return 0;
+                }
+                pre = i;
+            }
+            r[i] = pre;
+        }
+    }
+
+    VI ans;
+    int cur = N;
+    while (cur > 0) {
+        if (cur <= M) {
+            ans.EB(cur);
+            cur = 0;
+        } else {
+
+            const int target = cur - M;
+            if (S[target] == '0') {
+                ans.EB(M);
+                cur = target;
+            } else {
+                ans.EB(cur - r[target]);
+                cur = r[target];
+            }
+
+        }
+    }
+
+    REVERSE(ans);
+    REP(i, ans.size()) {
+        printf("%d%c", ans[i], (i + 1 == (int)ans.size() ? '\n' : ' '));
+    }
+}

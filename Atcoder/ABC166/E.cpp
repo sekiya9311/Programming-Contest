a8f2dff0fc8149c0384f9facee40db625a4c088a
sekiya9311 <sekiya9311@gmail.com>
2020-05-03 22:40:52 +0900

    ABC166

---
+++ Atcoder/ABC166/E.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define EB emplace_back
+#define GREATER(T) T, VT<T> greater<T>
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 2e5 + 10;
+int N;
+LL A[MAX];
+
+void naive() {
+    int cnt = 0;
+    REP(i, N) {
+        FOR(j, i + 1, N) {
+            if (abs(i - j) == A[i] + A[j]) {
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt << endl;
+}
+
+int main(void) {
+    cin >> N;
+    REP(i, N) {
+        cin >> A[i];
+    }
+
+    VI idx(N);
+    iota(ALL(idx), 0);
+    sort(ALL(idx), [&](const auto &l, const auto &r) {
+        return A[l] - l < A[r] - r;
+    });
+
+    map<LL, int> cnt;
+    LL ans = 0;
+    FORE(i, idx) {
+        ans += cnt[i + A[i]];
+        cnt[i - A[i]]++;
+    }
+
+    cout << ans << endl;
+}

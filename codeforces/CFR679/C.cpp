f8e6383d8689dd2dfa41351bc1c0758b499e0519
sekiya9311 <sekiya9311@gmail.com>
2020-10-25 22:48:40 +0900

    CFR679 C

---
+++ codeforces/CFR679/C.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define EB emplace_back
+#define GREATER(T) T, VT<T> greater<T>
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 1e5 + 10;
+LL a[6];
+int N;
+LL b[MAX];
+
+void solve() {
+    REP(i, 6) {
+        scanf("%lld", a + i);
+    }
+    sort(a, a + 6);
+    scanf("%d", &N);
+    REP(i, N) {
+        scanf("%lld", b + i);
+    }
+    sort(b, b + N);
+
+    VT<PLL> vec;
+    REP(i, N) {
+        REP(j, 6) {
+            vec.EB(b[i] - a[j], i);
+        }
+    }
+    SORT(vec);
+
+    LL ans = LLINF;
+    int j = 0;
+    int already = 0;
+    VL cnt(N);
+    REP(idx, vec.size()) {
+        const auto [v, i] = vec[idx];
+
+        cnt[i]++;
+        if (cnt[i] == 1) {
+            already++;
+        }
+
+        while (j < idx && cnt[vec[j].second] > 1) {
+            cnt[vec[j].second]--;
+            j++;
+        }
+
+        if (already == N) {
+            chmin(ans, v - vec[j].first);
+        }
+    }
+
+    printf("%lld\n", ans);
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    const auto in_stream = freopen("../in.txt", "r", stdin);
+    if (in_stream == nullptr) {
+        cerr << "ERROR!" << endl;
+        return 1;
+    }
+#endif
+
+    solve();
+
+#ifndef ONLINE_JUDGE
+    fclose(in_stream);
+#endif
+}

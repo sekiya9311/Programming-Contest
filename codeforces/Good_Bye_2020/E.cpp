bac05a627ee26ce8fbf6ec1bdef68178437ed4cd
sekiya9311 <sekiya9311@gmail.com>
2020-12-31 19:03:23 +0900

    Good Bye 2020

---
+++ codeforces/Good_Bye_2020/E.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <complex>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <bitset>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <cstddef>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
+#include <sstream>
+#include <fstream>
+#include <functional>
+
+using namespace std;
+#define REP(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); (i)++)
+#define RREP(i, a) for(int (i) = (a) - 1; (i) >= 0; (i)--)
+#define FORR(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define DEBUG(C) cerr << #C << " = " << C << endl;
+using LL = long long;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VL = vector<LL>;
+using VVL = vector<VL>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using PII = pair<int, int>;
+using PDD = pair<double, double>;
+using PLL = pair<LL, LL>;
+using VPII = vector<PII>;
+template<typename T> using VT = vector<T>;
+#define ALL(a) begin((a)), end((a))
+#define RALL(a) rbegin((a)), rend((a))
+#define SORT(a) sort(ALL((a)))
+#define RSORT(a) sort(RALL((a)))
+#define REVERSE(a) reverse(ALL((a)))
+#define MP make_pair
+#define FORE(a, b) for (auto &&a : (b))
+#define EB emplace_back
+#define GREATER(T) T, VT<T>, greater<T>
+template<typename T> inline bool chmax(T &a, T b){if (a < b){a = b;return true;}return false;}
+template<typename T> inline bool chmin(T &a, T b){if (a > b){a = b;return true;}return false;}
+
+const int INF = 1e9;
+const int MOD = INF + 7;
+const LL LLINF = 1e18;
+
+const int MAX = 5e5 + 10;
+
+// 剰余計算,MOD計算,順列・組み合わせ,Permutation・Combination
+
+template<int mod>
+class mod_calc {
+private:
+    const int defnum = -1;
+public:
+    mod_calc() { }
+    const inline int add(int a, const int b) const {
+        a += b;
+        while (a < 0) a += mod;
+        while (a >= mod) a -= mod;
+        return a;
+    }
+    const inline int mul(const int a, const int b) const {
+        return (long long)a * b % mod;
+    }
+    const inline void add_ref(int &a, const int b) const {
+        a = this->add(a, b);
+    }
+    const inline void mul_ref(int &a, const int b) const {
+        a = this->mul(a, b);
+    }
+    const inline void add_ref(long long &a, const int b) const {
+        a = this->add(a, b);
+    }
+    const inline void mul_ref(long long &a, const int b) const {
+        a = this->mul(a, b);
+    }
+    const inline int pow(int a, int p) const {
+        int res = 1;
+        while (p > 0) {
+            if (p & 1) res = this->mul(res, a);
+            a = this->mul(a, a);
+            p >>= 1;
+        }
+        return res;
+    }
+    const inline int inverse(int a) const {
+        return this->pow(a, mod - 2);
+    }
+};
+mod_calc<MOD> mc;
+
+int N;
+LL x[MAX];
+
+void naive() {
+    LL ans = 0;
+    REP(i, N) REP(j, N) REP(k, N) {
+        const auto l = (x[i] & x[j]) % MOD;
+        const auto r = (x[j] | x[k]) % MOD;
+        (ans += (l * r) % MOD) %= MOD;
+    }
+    printf("NAIVE: %lld\n", ans);
+}
+
+void solve2() {
+    scanf("%d", &N);
+    REP(i, N) {
+        scanf("%lld", x + i);
+    }
+
+    // naive();
+    int cnt[60] = {};
+    REP(i, N) {
+        REP(j, 60) {
+            if (x[i] & (1LL << j)) {
+                cnt[j]++;
+            }
+        }
+    }
+
+    LL ans = 0;
+    REP(j, N) {
+        LL cur_and = 0, cur_or = 0;
+        REP(bit, 60) {
+            const auto v = (1LL << bit) % MOD;
+            if (x[j] & (1LL << bit)) {
+                (cur_or += v * N % MOD) %= MOD;
+                (cur_and += v * cnt[bit] % MOD) %= MOD;
+            } else {
+                (cur_or += v * cnt[bit] % MOD) %= MOD;
+            }
+        }
+        const auto cur_add = cur_and * cur_or % MOD;
+        (ans += cur_add) %= MOD;
+    }
+
+    printf("%lld\n", ans);
+}
+
+void solve() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        solve2();
+    }
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    const auto in_stream = freopen("../in.txt", "r", stdin);
+    if (in_stream == nullptr) {
+        cerr << "ERROR!" << endl;
+        return 1;
+    }
+#endif
+
+    solve();
+
+#ifndef ONLINE_JUDGE
+    fclose(in_stream);
+#endif
+}
